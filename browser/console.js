544d6dfae337ad9c9c54f07cfc2eab89bb2fd9fd
Cameron Lakenen <lakenen@box.com>
2014-08-08 12:00:01 -0700

    Better console

--- browser/console.js
+++ browser/console.js
@@ -2,29 +2,29 @@ var util = require('util')
   , el = document.querySelector('.console')
   , PREFIX = '$&gt; '
 
-module.exports = {
+var console = {
     info: function () {
-      var html = util.format.apply(this, arguments)
+      var html = info(arguments)
       this.print(PREFIX + addClass(html, 'info'))
     }
   , debug: function () {
-      var html = util.format.apply(this, arguments)
+      var html = debug(arguments)
       this.print(PREFIX + addClass(html, 'debug'))
     }
   , warn: function () {
-      var html = util.format.apply(this, arguments)
+      var html = warn(arguments)
       this.print(PREFIX + addClass(html, 'warn'))
     }
   , error: function () {
-      var html = util.format.apply(this, arguments)
+      var html = error(arguments)
       this.print(PREFIX + addClass(html, 'error'))
     }
   , ok: function () {
-      var html = util.format.apply(this, arguments)
+      var html = log(arguments)
       this.print(PREFIX + addClass(html, 'ok'))
     }
   , log: function () {
-      var html = util.format.apply(this, arguments)
+      var html = log(arguments)
       this.print(PREFIX + addClass(html, 'log'))
     }
   , print: function (html) {
@@ -33,11 +33,36 @@ module.exports = {
     }
 }
 
-function addClass(html, c) {
-  return '<span class="' + c + '">' + html + '</span>'
-}
-
 if (!window._console) {
   window._console = window.console
-  window.console = module.exports
+  window.console = console
+}
+
+function format() {
+  return util.format.apply(this, arguments)
+}
+
+function error(args) {
+  window._console.error.apply(window._console, args)
+  return format.apply(null, args)
+}
+function info(args) {
+  window._console.info.apply(window._console, args)
+  return format.apply(null, args)
+}
+function log(args) {
+  window._console.log.apply(window._console, args)
+  return format.apply(null, args)
+}
+function warn(args) {
+  window._console.warn.apply(window._console, args)
+  return format.apply(null, args)
+}
+function debug(args) {
+  window._console.debug.apply(window._console, args)
+  return format.apply(null, args)
+}
+
+function addClass(html, c) {
+  return '<span class="' + c + '">' + html + '</span>'
 }
